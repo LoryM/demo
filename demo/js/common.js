27d4a87294599632562863775018f82b7d532f99
Lory <ms.lysochenko@mail>
2021-03-16 00:41:09 +0500

    demo3

--- demo/js/common.js
+++ demo/js/common.js
@@ -1,18 +1,14 @@
 $(document).ready(function() { 
 
   var input = document.querySelector('.input');
- //var value = input.('Поиск по сайту');
-
 
   $(input).on("mouseenter", function(){ 
     if ($(':focus').is(input)) { 
       $(this).val(''); return false; }
-    //input.placeholder = 'Название поиск|';
     $(this).val('Название поиск|').css('color','#000')
   }).on("mouseleave", function(){
     if ($(':focus').is(input)) { 
       $(this).val(''); return false; }
-    //input.placeholder = placeholder;
     $(this).val('Поиск по сайту').css('color','#0082c8')
   });
 
@@ -24,9 +20,7 @@ $('.main__slider').slick({
     //autoplay: true,
     //autoplaySpeed: 2000,
     arrows:true,
-    //arrows:true,
     slidesToShow: 1,
-    //fade:true,
     slidesToScroll: 1,
     nextArrow:'<div class="slide__arrow_right"></div>', 
     prevArrow: '<div class="slide__arrow_left"></div>',
@@ -39,7 +33,6 @@ $('.bottom__slider').slick({
     arrows:true,
     slidesToShow: 5,
     slidesToScroll: 1,
-    //useTransforms: false,
     useCSS: false,
     nextArrow:'<div class="slide__arrow_right arrow_right_bottom"></div>', 
     prevArrow: '<div class="slide__arrow_left arrow_left_bottom"></div>',
